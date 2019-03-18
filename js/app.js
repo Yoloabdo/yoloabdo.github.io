f506543e3b6085ed74c31cdfba565c7dde301ef0
Adel <mh4md.a@gmail.com>
2019-03-18 19:58:59 +0200

    Create workshop page

--- js/app.js
+++ js/app.js
@@ -9,4 +9,18 @@ $(document).ready(function() {
 	console.log('%cSketched, Coded and Deployed in less than 3 hours, by Mohamed Adel @d3vma!', 'background: #000; color: #00FF00; font-size: 15px;');
 	console.log('http://madel.me', '||', 'http://twitter.com/d3vma', '||', 'http://linkedin.com/in/d3vma');
 	$('.portfolio-screen-wrapper').height($('.portfolio-wrapper').outerHeight(true));
+
+	$(".owl-carousel").owlCarousel({
+		center: true,
+	    items:1,
+	    loop:true,
+	    margin:10,
+	    autoplay: true,
+	    autoplayTimeout: 3000,
+	    responsive:{
+	        600:{
+	            items: 1
+	        }
+	    }
+	});
 });
